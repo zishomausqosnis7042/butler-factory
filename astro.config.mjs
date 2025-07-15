fdda74fb3ce4c1d63b0df7936088b9079b96ff4f
zishomausqosnis7042 <zishomausqosnis7042@gmail.com>
2025-07-15 10:13:10 +0000

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,5 +2,5 @@ import { defineConfig } from 'astro/config';
 
 export default defineConfig({
   output: 'static',
-  site: 'https://ВАШ-БУДУЩИЙ-АДРЕС.netlify.app' // Мы заменим это позже
+  site: 'https://ВАШ-БУДУЩИЙ-АДРЕС.netlify.app' // Мы заменим это на ваш реальный адрес Netlify позже
 });
